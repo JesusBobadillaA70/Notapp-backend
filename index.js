4047693d4ce22827480ba9c29f9fa4b3f80bc0c8
Jesus Bobadilla <jesus.bobadilla@metricamovil.com>
2024-10-27 19:40:46 -0600

    Second commit

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ let notes = [
 ]
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+    response.send('<h1>I love Niñalinda</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
